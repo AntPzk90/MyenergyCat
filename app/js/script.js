f549ecb7c7029ce4e82c01771a056b6985fbbcda
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2018-11-05 17:41:36 +0100

    Add files via upload

--- app/js/script.js
+++ app/js/script.js
@@ -2,12 +2,13 @@ var range = document.querySelector(".range");
 var fatCat = document.querySelector(".fat-cat");
 range.addEventListener("change", function() {
   var valueNow = this.value;
-  if(valueNow < 150){
+ /* if(valueNow < 50){
   	valueNow = 0;
   	range.setAttribute("value", 0);
-  }else if(valueNow > 510){
+  }else if(valueNow > 600){
   	valueNow = 650;
   	range.setAttribute("value", 650);
-  }
+  }*/
   fatCat.style.width = valueNow +"px";
 });
+

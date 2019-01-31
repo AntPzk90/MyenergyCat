991aeb06f074d649e189d1ba4333e6e215f68947
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-01-31 14:59:37 +0300

    Add files via upload

--- script/js.js
+++ script/js.js
@@ -1,18 +1,1 @@
-var range = document.querySelector(".range");
-var fatCat = document.querySelector(".fat-cat");
-var slimCat = document.querySelector(".slim-cat");
-range.addEventListener("change", function() {
-  var valueNow = this.value;
- if(valueNow < 350){
-  	valueNow = 0;
-  	slimCat.style = "z-index:0";
-    fatCat.style.width = 600 + "px";
-  	range.setAttribute("value", 0);
-  }else if(valueNow > 350){
-  	valueNow = 650;
-  	range.setAttribute("value", 650);
-  	slimCat.style = "z-index:1";
-    fatCat.style.width = 0 + "px";
-  }
-  console.log(valueNow)
-});
+var mobyBtn=$(".byrger"),navList=$(".main__nav"),header=$(".main__header");console.log(mobyBtn),mobyBtn.on("click",function(a){a.preventDefault(),mobyBtn.hasClass("x")?(mobyBtn.removeClass("x"),mobyBtn.addClass("byrger"),navList.removeClass("main--active"),header.removeAttr("style")):(mobyBtn.addClass("x"),mobyBtn.removeClass("byrger"),navList.addClass("main--active"),header.css("padding-bottom",204),console.log(2))});
